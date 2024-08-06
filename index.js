f3f05cab656d0cdd6fc97192a4dfd764f4528ea2
Bharathimanju <166109876+Bharathimanju@users.noreply.github.com>
2024-08-06 11:33:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,222 +1,222 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Sample()
-{
-    return(
+// function Sample()
+// {
+//     return(
         
-        <div>
-            <label>Name</label>
-            <input type="text" placeholder="Enter yur name"></input>
-            <br/> <br/>
-            <label>Password</label>
-            <input type="password" placeholder="Enter the passord"></input>
-            <br/><br/>
+//         <div>
+//             <label>Name</label>
+//             <input type="text" placeholder="Enter yur name"></input>
+//             <br/> <br/>
+//             <label>Password</label>
+//             <input type="password" placeholder="Enter the passord"></input>
+//             <br/><br/>
         
-            <button>submit</button>
+//             <button>submit</button>
             
-        </div>
-    )
-}
-const r1= ReactDOM.createRoot(document.getElementById("root"))
-r1.render(<Sample/>)
+//         </div>
+//     )
+// }
+// const r1= ReactDOM.createRoot(document.getElementById("root"))
+// r1.render(<Sample/>)
 
-......................................[  1  ]....................................................
-
-
-Basic Rendering
-
-const Sample=()=>{
-    return(
-        <div>
-            <h1>my first react app</h1>
-            <p>welcme to you</p>
-        </div>
-    )
-}
-const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<Sample/>)
-
-...........................................[  2   ]..............................................
+// ......................................[  1  ]....................................................
 
 
-const App=()=>{
-    const username="manju"
-    const password="12345"
+// Basic Rendering
 
-    return(
-        <div>
-            <h1>my first react app.</h1>
-            <h2>to show my user name is {username} and password is {password} </h2>
-            <p>{new Date().toLocaleDateString()}</p>
-            <p>this is react page</p>
-            <label>username</label>
-            <input type="text" placeholder="Enter your name" />
-            <br></br>
-            <label>password</label>
-            <input type="password" placeholder='enter the pasword'></input>
-            <br></br>
-            <button>submit</button>
-        </div>
-    )
-}
-const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<App/>)
+// const Sample=()=>{
+//     return(
+//         <div>
+//             <h1>my first react app</h1>
+//             <p>welcme to you</p>
+//         </div>
+//     )
+// }
+// const r1=ReactDOM.createRoot(document.getElementById('root'))
+// r1.render(<Sample/>)
 
-.........................................[  3  ].........................................
+// ...........................................[  2   ]..............................................
 
-const myname1=<h1>welcome</h1>
-const myname2=<p>React is a javascript library for building user interface</p>
-const myname3=<label>username</label>
-const myname4=<input type="text" placeholder='Enter your name'/>
-const myname5=<label>password</label>
-const myname6=<input type="password" placeholder='Enter your password'/>
-const myname7=<button>submit</button>
-const myname8=<div>
-    <h1>My first react app</h1>
-    <p>{new Date().toLocaleTimeString()}</p>
-    {myname1}
-    {myname2}
-    <br/>
-    {myname3}
-    {myname4}
-    <br/>
-    {myname5}
-    {myname6}
-    <br/>
-    {myname7}
+
+// const App=()=>{
+//     const username="manju"
+//     const password="12345"
+
+//     return(
+//         <div>
+//             <h1>my first react app.</h1>
+//             <h2>to show my user name is {username} and password is {password} </h2>
+//             <p>{new Date().toLocaleDateString()}</p>
+//             <p>this is react page</p>
+//             <label>username</label>
+//             <input type="text" placeholder="Enter your name" />
+//             <br></br>
+//             <label>password</label>
+//             <input type="password" placeholder='enter the pasword'></input>
+//             <br></br>
+//             <button>submit</button>
+//         </div>
+//     )
+// }
+// const r1=ReactDOM.createRoot(document.getElementById('root'))
+// r1.render(<App/>)
+
+// .........................................[  3  ].........................................
+
+// const myname1=<h1>welcome</h1>
+// const myname2=<p>React is a javascript library for building user interface</p>
+// const myname3=<label>username</label>
+// const myname4=<input type="text" placeholder='Enter your name'/>
+// const myname5=<label>password</label>
+// const myname6=<input type="password" placeholder='Enter your password'/>
+// const myname7=<button>submit</button>
+// const myname8=<div>
+//     <h1>My first react app</h1>
+//     <p>{new Date().toLocaleTimeString()}</p>
+//     {myname1}
+//     {myname2}
+//     <br/>
+//     {myname3}
+//     {myname4}
+//     <br/>
+//     {myname5}
+//     {myname6}
+//     <br/>
+//     {myname7}
     
-</div>
-const r2=ReactDOM.createRoot(document.getElementById('root'));
-r2.render(myname8)
+// </div>
+// const r2=ReactDOM.createRoot(document.getElementById('root'));
+// r2.render(myname8)
 
-...........................................[  4  ]........................................................
+// ...........................................[  4  ]........................................................
 
-react list
-const App=()=>{
-    return(
-        <div>
-            <h1>welcome</h1>
-            <ul>
-                <li>one</li>
-                <li>two</li>
-                <li>three</li>
-            </ul>
-        </div>
-    )
-}
-const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<App/>)
+// react list
+// const App=()=>{
+//     return(
+//         <div>
+//             <h1>welcome</h1>
+//             <ul>
+//                 <li>one</li>
+//                 <li>two</li>
+//                 <li>three</li>
+//             </ul>
+//         </div>
+//     )
+// }
+// const r1=ReactDOM.createRoot(document.getElementById('root'))
+// r1.render(<App/>)
 
-.............................................[  5  ].................................................
+// .............................................[  5  ].................................................
 
-Fragment tag
+// Fragment tag
 
-const App=()=>{
-    return(
-        <>
-        <h1>welcome</h1>
-        <h2>welcome</h2>
-        </>
-    )
-}
-const r1= ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<App/>)
+// const App=()=>{
+//     return(
+//         <>
+//         <h1>welcome</h1>
+//         <h2>welcome</h2>
+//         </>
+//     )
+// }
+// const r1= ReactDOM.createRoot(document.getElementById('root'))
+// r1.render(<App/>)
 
-=============================================[  6  ]==================================================
+// =============================================[  6  ]==================================================
 
-import './index.css'
-const App=()=>{
-    return(
-        <div className='container'>
-            <div className='one'>
-                <h3>hello</h3>
-                <h4>nice to meet you</h4>
+// import './index.css'
+// const App=()=>{
+//     return(
+//         <div className='container'>
+//             <div className='one'>
+//                 <h3>hello</h3>
+//                 <h4>nice to meet you</h4>
 
-            </div>
-            <h1>welcome</h1>
-            <h2>one</h2>
-            <form>
-                <input type="button" value="submit" className="btn-btn-dark"/>
-            </form>
+//             </div>
+//             <h1>welcome</h1>
+//             <h2>one</h2>
+//             <form>
+//                 <input type="button" value="submit" className="btn-btn-dark"/>
+//             </form>
 
-        </div>
-    )
-}
-const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<App/>)
+//         </div>
+//     )
+// }
+// const r1=ReactDOM.createRoot(document.getElementById('root'))
+// r1.render(<App/>)
 
-=================================================[  7  ]=================================================
+// =================================================[  7  ]=================================================
 
 
-Login Form
-import './index.css'
+// Login Form
+// import './index.css'
 
-const App=()=>{
-    return(
-        <div className='container'>
-            <div className='box'>
-                <h1>Login</h1>
-                <form>
-                    <div className='inputbox'>
-                      <label>Name :</label>
-                      <hr></hr><br/>
-                       <label>Password:</label>
-                      <hr></hr>
-                    </div>
-                    <br/>
-                    <div className='forgot'>
-                        <label><input type="checkbox"/> Remember me</label>
-                        <a href='#'>forgot Password</a>
-                    </div> <br/>
-                    <button type='submit' className='btn'  >Login</button><br/>
-                    <div className='register'>
-                        <p> Dont't have an accound ? <a href='#'>Register</a></p>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
-}
-const r1=ReactDOM.createRoot(document.getElementById('root'))
-r1.render(<App/>)
+// const App=()=>{
+//     return(
+//         <div className='container'>
+//             <div className='box'>
+//                 <h1>Login</h1>
+//                 <form>
+//                     <div className='inputbox'>
+//                       <label>Name :</label>
+//                       <hr></hr><br/>
+//                        <label>Password:</label>
+//                       <hr></hr>
+//                     </div>
+//                     <br/>
+//                     <div className='forgot'>
+//                         <label><input type="checkbox"/> Remember me</label>
+//                         <a href='#'>forgot Password</a>
+//                     </div> <br/>
+//                     <button type='submit' className='btn'  >Login</button><br/>
+//                     <div className='register'>
+//                         <p> Dont't have an accound ? <a href='#'>Register</a></p>
+//                     </div>
+//                 </form>
+//             </div>
+//         </div>
+//     )
+// }
+// const r1=ReactDOM.createRoot(document.getElementById('root'))
+// r1.render(<App/>)
 
-=======================================[  8  ]===============================================
+// =======================================[  8  ]===============================================
 
-const x=11
-let text=""
-if(x>10){
-    text='world'
-}
-else if(x==10){
-    text="equal"
-}
-else{
-    text="less"
-}
-const Myelem=()=>{
-    return(
-        <div>
-            <h1>{text}</h1>
-        </div>
-    )
-}
-const r2=ReactDOM.createRoot(document.getElementById('root'))
-r2.render(<Myelem/>)
+// const x=11
+// let text=""
+// if(x>10){
+//     text='world'
+// }
+// else if(x==10){
+//     text="equal"
+// }
+// else{
+//     text="less"
+// }
+// const Myelem=()=>{
+//     return(
+//         <div>
+//             <h1>{text}</h1>
+//         </div>
+//     )
+// }
+// const r2=ReactDOM.createRoot(document.getElementById('root'))
+// r2.render(<Myelem/>)
 
-// // ==============bb==================[  9  ]====================================================
-// class component
-class Sample extends React.Component
-{
-    render()
-    {
-        return(
-            <div><h1>Welcome</h1></div>
-        )
-    }
-}
-const r3=ReactDOM.createRoot(document.getElementById('root'))
-r3.render(<Sample/>)
+// // // ==============bb==================[  9  ]====================================================
+// // class component
+// class Sample extends React.Component
+// {
+//     render()
+//     {
+//         return(
+//             <div><h1>Welcome</h1></div>
+//         )
+//     }
+// }
+// const r3=ReactDOM.createRoot(document.getElementById('root'))
+// r3.render(<Sample/>)
 // // ===================================[  10  ]============================================================
 
 // function component
@@ -296,9 +296,186 @@ r3.render(<Sample/>)
 // r5.render(<Branch/>)
 
 // ====================================[  14  ]=============================================
-import Navbar from './App.js'
-const r5=ReactDOM.createRoot(document.getElementById('root'))
-r5.render(<Navbar/>)
+//  import Navbar from './App.js'
+//  const r5=ReactDOM.createRoot(document.getElementById('root'))
+//  r5.render(<Navbar/>)
+
+// ==================================[  15 ]=================================================
+
+// constructor component
+
+class Sample extends React.Component
+{
+    constructor(){
+        super()
+        this.state={name:"rahul" ,
+            age:10}
+    }
+    render(){
+        return(
+            <div>
+                <h1>{this.state.name}</h1>
+                <h1>{this.state.age}</h1>
+                
+
+            </div>
+        )
+    }
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Sample/>)
+
+=====================[  16  ]======================================
+
+
+class Header extends Component
+{
+    text='welcome'
+    constructor(){
+        super();
+        this. state={f1:'blue'}
+    }
+    render()
+    {
+        return(
+            <div>
+                <h1>{this.text}</h1>
+                <h1>{this.state.f1}</h1>
+                <h1 style={{color:this.state.f1}}>welcome</h1>
+            </div>
+
+        )
+    }
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Header/>)
+========================[ 17 ]===============================================
+
+
+// changing the state object
+
+class Sample extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            emp: {
+                name: "kavi",
+                age: 20,
+                city: "chennai"
+            },
+            showData: false
+        };
+    }
+
+    showData() {
+        this.setState({ showData: true });
+    }
+
+    hideData() {
+        this.setState({ showData: false });
+    }
+
+    render() {
+        let data;
+        if (this.state.showData==true) {
+            data = 
+                <div>
+                    <b>name: </b> {this.state.emp.name}<br/> <b>age: </b> {this.state.emp.age} <br/><b>city: </b> {this.state.emp.city}<br/>
+                    <button onClick={this.hideData.bind(this)}>hideData</button>
+                </div>
+            
+        } else {
+            data = 
+                <div>
+                    <button onClick={this.showData.bind(this)}>showData</button>
+                </div>
+            
+        }
+
+        return (
+            <div>
+                <h1>welcome</h1>
+                <h2>Employee Details</h2>
+                {data}
+            </div>
+        );
+    }
+}
+
+const r1 = ReactDOM.createRoot(document.getElementById('root'));
+r1.render(<Sample />);
+
+// ============================[  18  ]===============================================
+
+// react Events
+
+function Football(){
+    const shoot=()=>{
+        alert("goal shooted")
+    }
+    return(
+        <div>
+            <button onClick={shoot}>shoot</button>
+        </div>
+    )
+}
+const r1 = ReactDOM.createRoot(document.getElementById('root'));
+r1.render(<Football/>);
+
+// ========================[  19  ]=========================================================
+
+function Football(){
+    const shoot=(a)=>{
+        alert(a)
+    }
+    return(
+        <div>
+            <button onClick={()=>shoot("goal shooted")}>shoot</button>
+        </div>
+    )
+}
+const r1 = ReactDOM.createRoot(document.getElementById('root'));
+r1.render(<Football/>);
+
+// ==================================[  19  ]===============================================
+
+// conditional rendering
+
+function Missedgoal()
+{
+    return<h1>MISSED</h1>
+}
+function Madegoal()
+{
+    return<h1>GOAL</h1>
+}
+function Goal(props){
+    const isGoal=props.isGoal;
+    if(isGoal)
+    {
+        return <Madegoal/>
+    }
+    else{
+        return  <Missedgoal/>
+    }
+}
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<Goal isGoal={Math.random()>0.5}/>)
+// ====================================[  20  ]=====================================================
+
+
+
+
+
+
+
+
+
+
+
+    
+
+                                                            
 
 
 
